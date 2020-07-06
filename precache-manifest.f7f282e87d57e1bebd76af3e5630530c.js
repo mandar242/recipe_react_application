cd389b5be202da34482e27063994363edd3d78dd
Mandar <mandar242@gmail.com>
2020-07-05 17:55:17 -0700

    Updates

---
+++ precache-manifest.f7f282e87d57e1bebd76af3e5630530c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4834e6cec975e8fee0f0c756bb3239",
+    "url": "/recipe_react_application/index.html"
+  },
+  {
+    "revision": "8275f981fbad33011f74",
+    "url": "/recipe_react_application/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "92337ef9ffcb89b40750",
+    "url": "/recipe_react_application/static/css/main.4661e502.chunk.css"
+  },
+  {
+    "revision": "8275f981fbad33011f74",
+    "url": "/recipe_react_application/static/js/2.d1112ee4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe_react_application/static/js/2.d1112ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92337ef9ffcb89b40750",
+    "url": "/recipe_react_application/static/js/main.a4d9cd4d.chunk.js"
+  },
+  {
+    "revision": "d5ab8f410011d163f45e",
+    "url": "/recipe_react_application/static/js/runtime-main.019055fe.js"
+  },
+  {
+    "revision": "8999a421ac849b5e480ffa17efe3fff8",
+    "url": "/recipe_react_application/static/media/ant.8999a421.jpg"
+  },
+  {
+    "revision": "b569b40c4f778bad22350c3ec01c6bd6",
+    "url": "/recipe_react_application/static/media/bg.b569b40c.jpg"
+  },
+  {
+    "revision": "f61df2e0fc73377500a1fd6a05c5565e",
+    "url": "/recipe_react_application/static/media/bw.f61df2e0.png"
+  },
+  {
+    "revision": "aeb2cb51c8bee2e073c3fd4c8e22424f",
+    "url": "/recipe_react_application/static/media/captain.aeb2cb51.jpg"
+  },
+  {
+    "revision": "36abc6cfb6381c9e72f99912895dbe74",
+    "url": "/recipe_react_application/static/media/doc.36abc6cf.jpg"
+  },
+  {
+    "revision": "260050835897fa0ce039c70e4ed2a7c6",
+    "url": "/recipe_react_application/static/media/hawk.26005083.png"
+  },
+  {
+    "revision": "c65e99d47e3cb3f1b0c3191738c45bdf",
+    "url": "/recipe_react_application/static/media/hulk.c65e99d4.jpg"
+  },
+  {
+    "revision": "39330ec46c475263ab7a06b6a1393dbb",
+    "url": "/recipe_react_application/static/media/iron.39330ec4.jpg"
+  },
+  {
+    "revision": "569128617ae353a3a85b134095b13adf",
+    "url": "/recipe_react_application/static/media/thor.56912861.png"
+  }
+]);
